397372b3cad494e4e03030d7412e9cef5469456b
qianteng <tengqian0001@gmail.com>
2019-10-23 23:08:11 -0400

    hw7 Qian Teng part2

--- hw7_QianTeng.cpp
+++ hw7_QianTeng.cpp
@@ -79,15 +79,151 @@ public:
 /***************************************************
  * 332. Reconstruct Itinerary
  * **************************************************/
+class Solution {
+public:
+    vector<string> findItinerary(vector<vector<string>>& tickets) {
+        unordered_map<string, multiset<string>> destinations;
+        stack<string> airports;
+        vector<string> itinerary;
+        for(auto ticket : tickets)
+            destinations[ticket[0]].insert(ticket[1]);
+        airports.push("JFK");
+        while(!airports.empty())
+        {
+            auto cur = airports.top();
+            if(destinations[cur].empty())
+            {
+                airports.pop();
+                itinerary.push_back(cur);
+            }  
+            else
+            {
+                string next = *destinations[cur].begin(); 
+                destinations[cur].erase(destinations[cur].begin());
+                airports.push(next);
+            }
+        }
+        reverse(itinerary.begin(), itinerary.end());
+        
+        return itinerary;
+    }
+};
 
 /***************************************************
  * 210. Course Schedule II
  * **************************************************/
+class Solution {
+public:
+    vector<int> findOrder(int numCourses, vector<vector<int>>& prerequisites) {
+        vector<vector<int>> childCourses(numCourses);
+        vector<int> numParentCourse(numCourses);
+        vector<int> rootCourses;
+        vector<int> result;
+        for(auto prerequisite : prerequisites)
+        {
+            childCourses[prerequisite[1]].push_back(prerequisite[0]);
+            ++numParentCourse[prerequisite[0]];
+        }
+        for(int i{0}; i < numCourses; ++i)
+        {
+            if(!numParentCourse[i])
+                rootCourses.push_back(i);
+        }
+        while(!rootCourses.empty())
+        {
+            int takeCourse = rootCourses.back();
+            rootCourses.pop_back();
+            result.push_back(takeCourse);
+            for(int childCourse : childCourses[takeCourse])
+                if(--numParentCourse[childCourse] == 0)
+                    rootCourses.push_back(childCourse);
+        }
+        
+        return result.size() == numCourses ? result : vector<int>();
+    }
+};
 
 /***************************************************
  * 269. Alien Dictionary
  * **************************************************/
+class Solution {
+public:
+    string alienOrder(vector<string>& words) {
+        set<char> chars;
+        unordered_map<char, set<char>> parent;
+        unordered_map<char, set<char>> child;
+        string first;
+        for(auto word : words)
+        {
+            for(int i{0}; i < min(first.size(), word.size()); ++i)
+            {
+                if(first[i] != word[i])
+                {
+                    parent[word[i]].insert(first[i]);
+                    child[first[i]].insert(word[i]);
+                    break;
+                }
+            }
+            chars.insert(word.begin(), word.end());
+            first = word;
+        }
+        set<char> free(chars);
+        string result;
+        for(auto p : parent)
+            free.erase(p.first);
+        while(!free.empty())
+        {
+            char a = *free.begin();
+            free.erase(a);
+            result += a;
+            for(char c : child[a])
+            {
+                parent[c].erase(a);
+                if(parent[c].empty())
+                    free.insert(c);
+            }
+        }
+        
+        return result.size() == chars.size() ? result : "";
+    }
+};
 
 /***************************************************
  * 1136. Parallel Courses
  * **************************************************/
+class Solution {
+public:
+    int minimumSemesters(int N, vector<vector<int>>& relations) {
+        vector<int> nPre(N + 1);
+        vector<vector<int>> child(N + 1);
+        for(auto r : relations)
+        {
+            child[r[0]].push_back(r[1]);
+            ++nPre[r[1]];
+        }
+        vector<int> free;
+        for(int i{0}; i <= N; ++i)
+        {
+            if(!nPre[i])
+                free.push_back(i);
+        }
+        int nSemsters{0};
+        int taken{0};
+        while(!free.empty())
+        {
+            ++nSemsters;
+            taken += free.size();
+            vector<int> nextFree;
+            for(auto f : free)
+            {
+                for(auto c : child[f])
+                {
+                    if(--nPre[c] == 0) nextFree.push_back(c);
+                }
+            }
+            free = nextFree;
+        }
+        
+        return taken == N + 1 ? nSemsters : -1;
+    }
+};
